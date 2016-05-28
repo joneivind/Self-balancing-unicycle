261a95579d034fb702d3139d054d3da359e629ca
Jon Eivind Stranden <joneivinds@gmail.com>
2016-05-28 19:44:20 +0200

    Update unicycle.ino

--- unicycle.ino
+++ unicycle.ino
@@ -1,45 +1,41 @@
 /*
 
-	***************************************
-	******* Self balancing unicycle *******
-	***************************************
+  ***************************************
+  ******* Self balancing unicycle *******
+  ***************************************
 
 
-	***** Connections *****
+  ***** Connections *****
 
-	* MPU6050  UNO/NANO
-	* VCC      +5v
-	* GND      GND
-	* SCL      A5
-	* SDA      A4
-	
-	* Motor Driver BTS7960
-	* VCC		+5v
-	* GND		GND
-	* RPWM		D5
-	* LPWM		D6
-	* R_EN		D7
-	* L_EN		D8
-	* B+		Battery+
-	* B-		Battery-
-	* M+		Motor+
-	* M-		Motor-
-	
-	* Voltage divider 0-24v -> 0-5v ( R1: 380k, R2: 100k)
-	* Vout		A3
+  * MPU6050  UNO/NANO
+  * VCC      +5v
+  * GND      GND
+  * SCL      A5
+  * SDA      A4
+  
+  * Motor Driver BTS7960
+  * VCC   +5v
+  * GND   GND
+  * RPWM    D5
+  * LPWM    D6
+  * R_EN    D7
+  * L_EN    D8
+  * B+    Battery+
+  * B-    Battery-
+  * M+    Motor+
+  * M-    Motor-
+  
+  * Voltage divider 0-24v -> 0-5v ( R1: 380k, R2: 100k)
+  * Vout    A3
 
-	***** Connections *****
+  ***** Connections *****
 
 
-	Credits MCU part: http://www.pitt.edu/~mpd41/Angle.ino
+  Credits MCU part: http://www.pitt.edu/~mpd41/Angle.ino
 
 */
 
 #include <Wire.h>
-
-#define degconvert 57.2957786 // There are 57 degrees in a radian
-#define pi = 3.14159265359; // Value for pi
-#define dt = (10.0/1000.0) // 100hz = 10ms
 
 // PID constants
 float kp = 5.0;
@@ -53,7 +49,7 @@ int max_pitch = 15; // Degrees before motor cut *SAFETY FUNCTION*
 int max_roll = 15; // Degrees before motor cut *SAFETY FUNCTION*
 
 // PID variables
-int Umax = 255;	 // Max output
+int Umax = 255;  // Max output
 int Umin = -255; // Min output
 
 float p_term = 0.0; // Store propotional value
@@ -63,10 +59,10 @@ float d_term = 0.0; // Store derivative value
 float error = 0.0; // Sum error
 float last_error = 0.0; // Store last error sum
 
-int output = 0.0;	// PID output
+int output = 0.0; // PID output
 
 // Timers
-int timer = millis();
+int main_loop_timer = millis();
 int lastTime = millis();
 
 // Input voltage divider
@@ -79,6 +75,8 @@ int R_EN = 7;
 int L_EN = 8;
 
 // MCU variables
+float degconvert = 57.2957786; // There are 57 degrees in a radian
+float dt = (10.0/1000.0); // 100hz = 10ms
 int MPU_addr = 0x68;
 int mcu_prev_dt; // MCU previous loop time
 float AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ; // The raw data from the MPU6050.
@@ -86,6 +84,8 @@ float roll = 0.0;
 float pitch = 0.0;
 float gyroXangle = 0.0;
 float gyroYangle = 0.0;
+float gyroXrate = 0.0;
+float gyroYrate = 0.0;
 float filtered_angle_pitch = 0.0;
 float filtered_angle_roll = 0.0;
 float mcu_dt = 0.0; // MCU loop time
@@ -95,24 +95,24 @@ float mcu_dt = 0.0; // MCU loop time
 // ***** PID function *****
 float pid(float measured_angle)
 {
-	float delta_t = millis() - lastTime; // Delta time
-	lastTime = millis(); // Reset timer
+  float delta_t = millis() - lastTime; // Delta time
+  lastTime = millis(); // Reset timer
 
-	error = setpoint - measured_angle; // Calculate error
+  error = setpoint - measured_angle; // Calculate error
 
-	p_term = kp * error;	// Propotional
-	i_term += (ki * error * delta_t);	// Integral
-	d_term = kd * ((error - last_error) / delta_t); // Derivative
+  p_term = kp * error;  // Propotional
+  i_term += (ki * error * delta_t); // Integral
+  d_term = kd * ((error - last_error) / delta_t); // Derivative
 
-	output = p_term + i_term + d_term; // Calculate output
+  output = p_term + i_term + d_term; // Calculate output
 
-	// Limit output
-	if (output > Umax) output = Umax;
-	else if (output < Umin) output = Umin;
-	
-	last_error = error; // Remember error for next time
+  // Limit output
+  if (output > Umax) output = Umax;
+  else if (output < Umin) output = Umin;
+  
+  last_error = error; // Remember error for next time
 
-	return output;
+  return output;
 }
 
 
@@ -120,163 +120,163 @@ float pid(float measured_angle)
 // ***** Motor output *****
 void motor(int pwm)
 {
-	// Set direction
-	if (pwm > (0 + deadband))
-	{	
-		// Drive forward
-		analogWrite(RPWM, abs(pwm)); 
-	}
-	else if (pwm < (0 - deadband))
-	{	
-		// Drive backward
-		analogWrite(LPWM, abs(pwm)); 
-	}
-	else
-	{	
-		// Stop motor
-		digitalWrite(RPWM, 0);
-		digitalWrite(LPWM, 0);
-	}
+  // Set direction
+  if (pwm > (0 + deadband))
+  { 
+    // Drive forward
+    analogWrite(RPWM, abs(pwm)); 
+  }
+  else if (pwm < (0 - deadband))
+  { 
+    // Drive backward
+    analogWrite(LPWM, abs(pwm)); 
+  }
+  else
+  { 
+    // Stop motor
+    digitalWrite(RPWM, 0);
+    digitalWrite(LPWM, 0);
+  }
 }
 
 
 
 // ***** Get angle from MPU6050 gyro/acc *****
 void get_angle() 
-{	
-	// Collect raw data from the sensor.
-	Wire.beginTransmission(MPU_addr);
-	Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-	Wire.endTransmission(false);
-	Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-	AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-	AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-	AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-	Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-	GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-	GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-	GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+{ 
+  // Collect raw data from the sensor.
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
-	mcu_dt = (float)(millis() - mcu_prev_dt) * 1000; //This line does three things: 1) stops the timer, 2)converts the timer's output to seconds from microseconds, 3)casts the value as a double saved to "dt".
-	mcu_prev_dt = millis(); //start the timer again so that we can calculate the next dt.
+  mcu_dt = (float)(millis() - mcu_prev_dt) * 1000; //This line does three things: 1) stops the timer, 2)converts the timer's output to seconds from microseconds, 3)casts the value as a double saved to "dt".
+  mcu_prev_dt = millis(); //start the timer again so that we can calculate the next dt.
 
-	//the next two lines calculate the orientation of the accelerometer relative to the earth and convert the output of atan2 from radians to degrees
-	//We will use this data to correct any cumulative errors in the orientation that the gyroscope develops.
-	roll = atan2(AcY, AcZ)*degconvert;
-	pitch = atan2(-AcX, AcZ)*degconvert;
+  //the next two lines calculate the orientation of the accelerometer relative to the earth and convert the output of atan2 from radians to degrees
+  //We will use this data to correct any cumulative errors in the orientation that the gyroscope develops.
+  roll = atan2(AcY, AcZ)*degconvert;
+  pitch = atan2(-AcX, AcZ)*degconvert;
 
-	//The gyroscope outputs angular velocities.  To convert these velocities from the raw data to deg/second, divide by 131.  
-	//Notice, we're dividing by a double "131.0" instead of the int 131.
-	gyroXrate = GyX/131.0;
-	gyroYrate = GyY/131.0;
+  //The gyroscope outputs angular velocities.  To convert these velocities from the raw data to deg/second, divide by 131.  
+  //Notice, we're dividing by a double "131.0" instead of the int 131.
+  gyroXrate = GyX/131.0;
+  gyroYrate = GyY/131.0;
 }
 
 
 
 // ***** Main setup *****
-void setup 
-{	
-	// ***** MPU6050 setup *****
-	Wire.begin();
-	#if ARDUINO >= 157
-	Wire.setClock(400000UL); // Set I2C frequency to 400kHz
-	#else
-	TWBR = ((F_CPU / 400000UL) - 16) / 2; // Set I2C frequency to 400kHz
-	#endif
+void setup()
+{ 
+  // ***** MPU6050 setup *****
+  Wire.begin();
+  #if ARDUINO >= 157
+  Wire.setClock(400000UL); // Set I2C frequency to 400kHz
+  #else
+  TWBR = ((F_CPU / 400000UL) - 16) / 2; // Set I2C frequency to 400kHz
+  #endif
 
-	Wire.beginTransmission(MPU_addr);
-	Wire.write(0x6B);  // PWR_MGMT_1 register
-	Wire.write(0);     // set to zero (wakes up the MPU-6050)
-	Wire.endTransmission(true);
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
 
-	delay(100);
+  delay(100);
 
-	// setup starting angle
-	//1) collect the data
-	Wire.beginTransmission(MPU_addr);
-	Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
-	Wire.endTransmission(false);
-	Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
-	AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-	AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-	AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-	Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-	GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-	GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-	GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+  // setup starting angle
+  //1) collect the data
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true);  // request a total of 14 registers
+  AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read();  // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read();  // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
-	//2) calculate pitch and roll
-	roll = atan2(AcY, AcZ)*degconvert;
-	pitch = atan2(-AcX, AcZ)*degconvert;
+  //2) calculate pitch and roll
+  roll = atan2(AcY, AcZ)*degconvert;
+  pitch = atan2(-AcX, AcZ)*degconvert;
 
-	//3) set the starting angle to this pitch and roll
-	gyroXangle = roll;
-	gyroYangle = pitch;
-	filtered_angle_roll = roll;
-	filtered_angle_pitch = pitch;
-
-
-	// ***** Read battery voltage *****
-	pinMode(battery_voltage_input, INPUT);
+  //3) set the starting angle to this pitch and roll
+  gyroXangle = roll;
+  gyroYangle = pitch;
+  filtered_angle_roll = roll;
+  filtered_angle_pitch = pitch;
 
 
-	// ***** Motorcontroller setup *****
-	pinMode(RPWM, OUTPUT); // PWM output right channel
- 	pinMode(LPWM, OUTPUT); // PWM output left channel
- 	pinMode(R_EN, OUTPUT); // Enable right channel
- 	pinMode(L_EN, OUTPUT); // Enable left channel
-
- 	digitalWrite(RPWM, LOW); // Disable motor @ start
- 	digitalWrite(LPWM, LOW);
- 	digitalWrite(R_EN, LOW);
- 	digitalWrite(L_EN, LOW);
+  // ***** Read battery voltage *****
+  pinMode(battery_voltage_input, INPUT);
 
 
+  // ***** Motorcontroller setup *****
+  pinMode(RPWM, OUTPUT); // PWM output right channel
+  pinMode(LPWM, OUTPUT); // PWM output left channel
+  pinMode(R_EN, OUTPUT); // Enable right channel
+  pinMode(L_EN, OUTPUT); // Enable left channel
 
-	// ***** Begin serial port *****
-	Serial.begin(115200);
+  digitalWrite(RPWM, LOW); // Disable motor @ start
+  digitalWrite(LPWM, LOW);
+  digitalWrite(R_EN, LOW);
+  digitalWrite(L_EN, LOW);
 
 
 
-	// ***** Initialize timer *****
-	mcu_prev_dt = millis(); // Initialize MCU timer
-	main_loop_timer = millis(); // Initialize main loop timer
+  // ***** Begin serial port *****
+  Serial.begin(115200);
+
+
+
+  // ***** Initialize timer *****
+  mcu_prev_dt = millis(); // Initialize MCU timer
+  main_loop_timer = millis(); // Initialize main loop timer
 }
 
 
 
 // ***** Main loop *****
-void loop 
-{	
-	if ((millis() - main_loop_timer) > (dt * 1000)) // Run loop @ 100hz (10ms)
-	{
-       	main_loop_timer = millis(); // Reset main loop timer
-       	
-       	//Read battery voltage and convert to 0-100%
-		int battery_voltage = map(analogRead(battery_voltage_input), 0, 1023, 0, 100);
+void loop()
+{ 
+  if ((millis() - main_loop_timer) > (dt * 1000)) // Run loop @ 100hz (10ms)
+  {
+        main_loop_timer = millis(); // Reset main loop timer
+        
+        //Read battery voltage and convert to 0-100%
+    int battery_voltage = map(analogRead(battery_voltage_input), 0, 1023, 0, 100);
 
-       	get_angle(); //Get angle from MCU6050
+        get_angle(); //Get angle from MCU6050
 
-       	// Calculate the angle using a Complimentary filter
-       	filtered_angle_roll = 0.98 * (filtered_angle_roll + gyroXrate * mcu_dt) + 0.02 * roll;  // X-axis
-		filtered_angle_pitch = 0.98 * (filtered_angle_pitch + gyroYrate * mcu_dt) + 0.02 * pitch; // Y-axis
-		
-		//Calculate PID output
-		int pid_output = pid(filtered_angle_pitch); // +-255
+        // Calculate the angle using a Complimentary filter
+        filtered_angle_roll = 0.98 * (filtered_angle_roll + gyroXrate * mcu_dt) + 0.02 * roll;  // X-axis
+    filtered_angle_pitch = 0.98 * (filtered_angle_pitch + gyroYrate * mcu_dt) + 0.02 * pitch; // Y-axis
+    
+    //Calculate PID output
+    int pid_output = pid(filtered_angle_pitch); // +-255
 
-    	// If xy angle is greater than max degrees, stop motor *SAFETY*
-    	if ((abs(filtered_angle_pitch) < max_pitch) && (abs(filtered_angle_roll) < max_roll))
-    	{
-			digitalWrite(R_EN,HIGH);
-			digitalWrite(L_EN,HIGH);
-			motor(pid_output); // Write PID output to motor
-		}
-		else
-		{	
-			// Stop motor
-			digitalWrite(R_EN,LOW);
-			digitalWrite(L_EN,LOW);
-			motor(0);
-		}
-	}
+      // If xy angle is greater than max degrees, stop motor *SAFETY*
+      if ((abs(filtered_angle_pitch) < max_pitch) && (abs(filtered_angle_roll) < max_roll))
+      {
+      digitalWrite(R_EN,HIGH);
+      digitalWrite(L_EN,HIGH);
+      motor(pid_output); // Write PID output to motor
+    }
+    else
+    { 
+      // Stop motor
+      digitalWrite(R_EN,LOW);
+      digitalWrite(L_EN,LOW);
+      motor(0);
+    }
+  }
 }
